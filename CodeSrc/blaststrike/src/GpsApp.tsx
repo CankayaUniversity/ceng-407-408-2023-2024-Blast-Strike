44d5230d587eda9b2ba65d9bd7d623ba01ebbce1
nadide-solmaz <c1911056@student.cankaya.edu.tr>
2024-03-22 21:32:32 +0300

    Add Haversine Distance Calculation

--- CodeSrc/blaststrike/src/GpsApp.tsx
+++ CodeSrc/blaststrike/src/GpsApp.tsx
@@ -4,10 +4,35 @@ import { StyleSheet, Text, View,Image } from 'react-native';
 import * as Location from "expo-location";
 import { useEffect, useState } from 'react';
 
+ 
+// Haversine
+function getDistance(lat1, lon1, lat2, lon2) {
+  const toRadians = (degrees) => {
+      return degrees * Math.PI / 180;
+  }
+
+  const earthRadius = 6371000;
+
+  const phi1 = toRadians(lat1);
+  const phi2 = toRadians(lat2);
+  const deltaPhi = toRadians(lat2 - lat1);
+  const deltaLambda= toRadians(lon2 - lon1);
+
+  const a = Math.sin(deltaPhi / 2) * Math.sin(deltaPhi / 2) +
+      Math.cos(phi1) * Math.cos(phi2) *
+      Math.sin(deltaLambda / 2) * Math.sin(deltaLambda / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  const distance = earthRadius * c;
+  return distance;
+}
+
 export default function App() {
   
   const [location,setLocation]=useState({})
   const [heading,setHeading]=useState({})
+  const [distance, setDistance] = useState({})
 
   useEffect(()=>{
     (async()=>{
@@ -25,6 +50,15 @@ export default function App() {
         console.log('Heading:', heading);
       });
       setHeading(heading)
+
+      const distance = getDistance(
+        location.coords.latitude,
+        location.coords.longitude,
+        34.0522, // default values must be changed
+      -118.2437 
+         
+    );
+    setDistance(distance);
     })()
     
   }, [])
@@ -33,6 +67,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>{JSON.stringify(location)}</Text>
       <Text>{JSON.stringify(heading)}</Text>
+      <Text>{JSON.stringify(distance)}</Text>
       <StatusBar style="auto" />
     </View>
   );
