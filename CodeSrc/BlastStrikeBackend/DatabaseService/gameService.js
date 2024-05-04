2618251f458269df0a48eee50c91b06af49975f5
Kaan Salt <kaansalt@hotmail.com>
2024-05-04 17:30:42 +0300

    *Backend improvement. *If a player is dead system will automaticly set dead player's health to 100 after 5 sec.

--- CodeSrc/BlastStrikeBackend/DatabaseService/gameService.js
+++ CodeSrc/BlastStrikeBackend/DatabaseService/gameService.js
@@ -20,18 +20,19 @@ async function hitPlayer(db, data) {
             // Access the field values from the document data
             const docData = docSnapshot.data();
 
-            
-            if(isDead(docData.teamBlue[0].health,damage))
+            //check enemy killed && score updates
+            if(isDead(docData[enemyTeam][0].health,damage))
             {
-                console.log("isdead");
-                console.log("data['playerTeam']",data.playerTeam);
+               // console.log("isdead");
+               // console.log("data['playerTeam']",data['playerTeam']);
+               // console.log("docData[enemyTeam][0]",docData[enemyTeam][0]);
                 
                 /// eğer can 0dan aşşağı düşer ise enemy canı tekrar 100 e çekilecek ve vuran takımın puanına +1 eklenicek
                 // vurulanın canı 5 saniye sonra tekrar 100 e çek awaitsiz update çağır 5 saniye sonra
                 newHealth=0;
                 if(data['playerTeam'] == "teamRed")// if red team player kills blue team member update score
                 {
-                    console.log("docData.scoreRed",docData.scoreRed);
+                //  console.log("docData.scoreRed",docData.scoreRed);
                     docData.scoreRed+=1;
                     await updateDoc(docRef, {
                         scoreRed:  docData.scoreRed
@@ -39,24 +40,28 @@ async function hitPlayer(db, data) {
                 }
                 else // if blue team player kills red team member update score
                 {
-                    console.log("docData.scoreBlue",docData.scoreBlue);
+                   // console.log("docData.scoreBlue",docData.scoreBlue);
                     docData.scoreBlue+=1;
                     await updateDoc(docRef, {
                         scoreBlue:  docData.scoreBlue
                     });
                 }
 
-                //playerTeam ın scoreunu arttır
-                /*
-                setTimeout(() => {   
-                    //vurulanın   canını 100 yap    
-                }
-                    , 5000); // Call myFunction after 5 seconds
-                    */
+
+                
+                setTimeout( async() => {   
+                    //vurulanın   canını 100 yap   
+                    await updateDoc(docRef, {
+                        [`${enemyTeam}.0.health`] :100
+                    });}, 5000); // Call myFunction after 5 seconds
+                    
             }
             else
+            {
                 newHealth=docData[enemyTeam][0].health-damage;
-            
+            }
+           //check enemy killed && score updates - END
+
             ////check if game ended 
             if(docData.scoreBlue==10 || docData.scoreRed==10)
             {
@@ -64,14 +69,20 @@ async function hitPlayer(db, data) {
                     inGame: false
                 });
             }
-                
+            else{
+                if( docData[enemyTeam][0].health > 0)
+                    {
+                    //update player health
+                    docData[enemyTeam][0].health = newHealth;
+                    //console.log("data",data);
+                    await updateDoc(docRef, {
+                        [`${enemyTeam}.0`]: docData[enemyTeam][0] 
+                    });
+                    }    
+            }
+   
+            
 
-            //update player  got killed health to 0
-            docData[enemyTeam][0].health = newHealth;
-            console.log("data",data);
-            await updateDoc(docRef, {
-                [`${enemyTeam}.0`]: docData[enemyTeam][0] 
-            });
 
         } else {
             console.log('Document does not exist');
@@ -86,8 +97,9 @@ async function hitPlayer(db, data) {
 
 // kill decide logic
 function isDead (enemyHealth,damage){
-    console.log("enemyHealth",enemyHealth);
-    if(enemyHealth-damage<=0)
+   // console.log("enemyHealth",enemyHealth);
+   // enemyHealth>0  to not kill currently dead player
+    if(enemyHealth>0 && enemyHealth-damage<=0)
         return true;
 
     return false;
