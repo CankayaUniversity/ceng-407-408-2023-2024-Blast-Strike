1e0949c64d9b905f64fc64550ca871d1e3e48ac0
nadide-solmaz <c1911056@student.cankaya.edu.tr>
2024-05-19 16:03:40 +0300

    Add calculate distance and bearing

--- CodeSrc/BlastStrikeBackend/DatabaseService/gameService.js
+++ CodeSrc/BlastStrikeBackend/DatabaseService/gameService.js
@@ -1,9 +1,13 @@
 import { collection, getDocs, getDoc,addDoc,query,where,doc,updateDoc} from 'firebase/firestore/lite';
 import { db } from './firebaseConfig.js';
+import { getDistance, getRhumbLineBearing } from 'geolib';
 
 async function hitPlayer(db, data) {
     let documentId = data['documentId'];
     let damage = data['damage'];
+    let playerLat = data['latitude'];
+    let playerLon = data['longitude'];
+    let playerHeading = data['heading'];
     let enemyTeam = data['playerTeam']=="teamBlue" ? "teamRed" :"teamBlue";  /// iki ikişilik demoda karıyı vurmak için
     //let enemyTeam = data['playerTeam'];
     console.log("enemyTeam", enemyTeam);
@@ -18,7 +22,30 @@ async function hitPlayer(db, data) {
         if (docSnapshot.exists()) {
             // Access the field values from the document data
             const data = docSnapshot.data();
+            const enemyData = data[enemyTeam][0];
+            const enemyLat = enemyData.locations._lat;
+            const enemyLon = enemyData.locations._long;
+            const enemyHeading = enemyData.heading.trueHeading;
 
+            console.log('Enemy Data:', enemyData);
+
+            const distance = getDistance(
+                { latitude: playerLat, longitude: playerLon },
+                { latitude: enemyLat, longitude: enemyLon }
+            );
+
+            const bearing = getRhumbLineBearing(
+            { latitude: playerLat, longitude: playerLon },
+            { latitude: enemyLat, longitude: enemyLon }
+            );
+
+            const angleDifference = Math.abs(playerHeading - bearing);
+
+            console.log('Distance:', distance);
+            console.log('Bearing:', bearing);
+            console.log('Angle Difference:', angleDifference);
+
+            
             //console.log("data[enemyTeam]",data[enemyTeam.toString()][0].health);
             data[enemyTeam][0].health -=damage;
             console.log("data",data);
